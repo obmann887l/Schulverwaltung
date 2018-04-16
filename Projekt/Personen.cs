102c13b796d6ca37cbce843f18e9ba01cf805957
obmann887l <34032198+obmann887l@users.noreply.github.com>
2018-04-16 15:28:32 +0200

    Projektdateien hinzufügen.

---
+++ Projekt/Personen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Personen
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public int Alter { get; set; }
+    }
+}
