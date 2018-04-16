f954012d8183b16080add33befbb3f4533183f07
obmann887l <34032198+obmann887l@users.noreply.github.com>
2018-04-16 15:35:21 +0200

    test

--- Projekt/Personen.cs
+++ Projekt/Personen.cs
@@ -8,6 +8,7 @@ namespace Projekt
 {
     class Personen
     {
+        //Test
         public string Vorname { get; set; }
         public string Nachname { get; set; }
         public int Alter { get; set; }
